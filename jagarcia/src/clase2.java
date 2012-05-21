95a96667230f60be8306a75ab769ffb87db52707
jagarcia_1º_Commit <jagarcia@106-07.info2>
2012-05-21 13:12:03 +0200

    13:12 21-05-2012 C2

---
+++ jagarcia/src/clase2.java
@@ -0,0 +1,17 @@
+
+public class clase2 {
+
+	/**
+	 * @param args
+	 */
+	public static void metodo1_clase2()	
+	{
+		System.out.println("Soy la clase 2");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		metodo1_clase2();	
+	}
+
+}
