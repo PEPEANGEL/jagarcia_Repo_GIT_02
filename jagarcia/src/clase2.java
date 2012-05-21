fc1b261163447d7fac66619388330774fd4624b1
jagarcia_1º_Commit <jagarcia@106-07.info2>
2012-05-21 13:38:16 +0200

    METODO1_1_CLASE2

--- jagarcia/src/clase2.java
+++ jagarcia/src/clase2.java
@@ -9,9 +9,16 @@ public class clase2 {
 		System.out.println("Soy la clase 2");
 	}
 	
+	public static void metodo1_1_clase2()	
+	{
+		System.out.println("Hola otra vez CLASE 2");
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		metodo1_clase2();	
+		metodo1_clase2();
+		
+		metodo1_1_clase2();
 	}
 
 }
